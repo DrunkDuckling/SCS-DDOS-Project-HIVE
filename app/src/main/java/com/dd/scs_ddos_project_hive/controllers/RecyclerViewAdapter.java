430b5d4444dc0e5998c74b9f47c9e57b29a324aa
DrunkDuckling <soerensen5492@gmail.com>
2020-12-16 21:02:08 +0100

    same same
    but different

--- app/src/main/java/com/dd/scs_ddos_project_hive/controllers/RecyclerViewAdapter.java
+++ app/src/main/java/com/dd/scs_ddos_project_hive/controllers/RecyclerViewAdapter.java
@@ -14,34 +14,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.dd.scs_ddos_project_hive.R;
+import com.dd.scs_ddos_project_hive.models.IPModel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private static final String TAG = "RecyclerViewAdapter";
 
-    private ArrayList<String> mip_name = new ArrayList<>();
-    private ArrayList<String> mip_mac = new ArrayList<>();
-    private ArrayList<String> mip_comp = new ArrayList<>();
-    private ArrayList<String> mip_addr = new ArrayList<>();
-    private ArrayList<String> mip_img = new ArrayList<>();
-    private Context mContext;
+    List<IPModel> mIPlist;
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> mip_name, ArrayList<String> mip_mac, ArrayList<String> mip_comp, ArrayList<String> mip_addr, ArrayList<String> mip_img) {
-        this.mip_name = mip_name;
-        this.mip_mac = mip_mac;
-        this.mip_comp = mip_comp;
-        this.mip_addr = mip_addr;
-        this.mip_img = mip_img;
-        this.mContext = context;
+    public RecyclerViewAdapter(List<IPModel> mIPlist) {
+        this.mIPlist = mIPlist;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_ip_list, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
+        return new ViewHolder(view);
     }
 
     @Override
@@ -52,11 +43,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mIPlist.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
         TextView txt_ip_name, txt_ip_mac, txt_ip_comp, txt_ip_addr;
         ImageView imageView_ip;
         RelativeLayout parentLayout;
@@ -69,7 +59,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             txt_ip_addr = itemView.findViewById(R.id.txt_ip_addr);
             imageView_ip = itemView.findViewById(R.id.imageView_ip);
             parentLayout = itemView.findViewById(R.id.parent);
-
         }
     }
 }
--- app/src/main/java/com/dd/scs_ddos_project_hive/models/IPModel.java
+++ app/src/main/java/com/dd/scs_ddos_project_hive/models/IPModel.java
@@ -2,8 +2,49 @@ package com.dd.scs_ddos_project_hive.models;
 
 public class IPModel {
     private String ip;
+    private String mac;
+    private String comp;
+    private String name;
 
-    public IPModel(String ip) {
+    public IPModel(String ip, String mac, String comp, String name) {
         this.ip = ip;
+        this.mac = mac;
+        this.comp = comp;
+        this.name = name;
+    }
+
+    public IPModel() {
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getComp() {
+        return comp;
+    }
+
+    public void setComp(String comp) {
+        this.comp = comp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
