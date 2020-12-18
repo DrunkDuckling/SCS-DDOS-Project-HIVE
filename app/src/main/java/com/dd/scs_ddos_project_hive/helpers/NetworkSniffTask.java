5814dde5ee7c4dca9e8b2329b119f4c52447a7de
DrunkDuckling <soerensen5492@gmail.com>
2020-12-18 10:25:05 +0100

    2345
    234

--- app/src/main/java/com/dd/scs_ddos_project_hive/helpers/NetworkSniffTask.java
+++ app/src/main/java/com/dd/scs_ddos_project_hive/helpers/NetworkSniffTask.java
@@ -9,8 +9,14 @@ import android.os.AsyncTask;
 import android.text.format.Formatter;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.dd.scs_ddos_project_hive.models.IPModel;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.InetAddress;
 import java.util.ArrayList;
@@ -85,6 +91,49 @@ public class NetworkSniffTask extends AsyncTask<Void, Void, List<IPModel>> {
     protected void onPostExecute(List<IPModel> s) {
         super.onPostExecute(s);
         CONSTANT.ASYNCTASKRUNNING = true;
+    }
+
+    public String getMacAddressFromIP(@NonNull String ipFinding, List<IPModel> antarDevicesInfos)
+    {
+
+        Log.i("IPScanning","Scan was started!");
+        //List<model> antarDevicesInfos = new ArrayList<>();
+
+
+        BufferedReader bufferedReader = null;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader("/proc/net/arp"));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4) {
+                    String ip = splitted[0];
+                    String mac = splitted[3];
+                    if (mac.matches("..:..:..:..:..:..")) {
+
+                        if (ip.equalsIgnoreCase(ipFinding))
+                        {
+                            return mac;
+                        }
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "00:00:00:00";
     }
 
 // TODO Testing stage, maybe do it. Hwo knows
